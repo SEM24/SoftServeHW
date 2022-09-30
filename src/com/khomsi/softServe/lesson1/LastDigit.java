dfdac7be86a634c8bf1be8baf5a494f7407343f9
SEM laptop <samerx777222@gmail.com>
2022-09-30 15:46:18 +0200

    Check if Last Digit is 3
    In this task we check if input has a digit 3.
    Used several methods to try different ways to success the task.
    Added getInput method that check the right user's input

---
+++ src/com/khomsi/softServe/lesson1/LastDigit.java
@@ -0,0 +1,63 @@
+/*
+ * Author: SEM
+ */
+
+package com.khomsi.softServe.lesson1;
+
+import java.util.Scanner;
+
+public class LastDigit {
+
+    public static void main(String[] args) {
+        new LastDigit().run();
+    }
+
+    private void run() {
+        Scanner scanner = new Scanner(System.in);
+        int num = getInput(scanner);
+        calculateDigit1(num);
+        calculateDigit2(num);
+        calculateDigit3(num);
+    }
+
+    /*
+       First solution
+     */
+    private void calculateDigit1(int num) {
+        boolean result = num % 10 == 3;
+        System.out.println(result ? "The last digit of number " + num + " is 3"
+                : "The last digit of number " + num + " isn't 3");
+    }
+
+    /*
+       Second solution
+     */
+    private void calculateDigit2(int num) {
+        int result = num % 10;
+        System.out.println(result == 3 ? "The last digit is " + result + " that fits our task"
+                : "The last digit is " + result + " that doesn't fit our task");
+    }
+
+    /*
+       Third solution
+     */
+    private void calculateDigit3(int num) {
+        String number = Integer.toString(num);
+        char result = number.charAt(number.length() - 1);
+        System.out.println("The last digit is " + result);
+    }
+
+    /*
+       Get the number from user
+     */
+    private int getInput(Scanner scanner) {
+        System.out.print("Print number: ");
+        int num;
+        while (!scanner.hasNextInt()) {
+            System.out.print("That's not a number! Try again: ");
+            scanner.next();
+        }
+        num = scanner.nextInt();
+        return num;
+    }
+}
