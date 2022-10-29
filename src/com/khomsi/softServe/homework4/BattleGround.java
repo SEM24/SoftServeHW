a526159b0de5bff051f11e455c5badb4b7ad1126
SEM laptop <samerx777222@gmail.com>
2022-10-29 23:43:59 +0200

    Code refactor

--- src/com/khomsi/softServe/homework4/BattleGround.java
+++ src/com/khomsi/softServe/homework4/BattleGround.java
@@ -4,19 +4,31 @@
 
 package com.khomsi.softServe.homework4;
 
-import com.khomsi.softServe.homework4.character_type.Enemy;
+import com.khomsi.softServe.homework4.enemies.Zombie;
 import com.khomsi.softServe.homework4.heroes.Mage;
 
 public class BattleGround {
+
+
     public static void main(String[] args) {
-        new BattleGround().startBattle();
+        new GameTools().startGame();
+//        new BattleGround().startBattle();
     }
+
 
     private void startBattle() {
+//        TODO
+//        Different enemies = different hp/damage
+//        Different heroes = different hp and damage (someone damages powerful, but has a low hp)
+//        Make the game more balanced
         Mage mage = new Mage("Vlad");
-        Enemy enemy = new Enemy(100);
+        Zombie zombie = new Zombie(100);
 
-        mage.attackEnemy(enemy);
-        System.out.println(enemy.isAlive() ? "Enemy is still alive" : "Enemy is dead");
+        mage.attackEnemy(zombie);
+        System.out.println(zombie.isAlive() ? "Enemy is still alive" : "Enemy is dead");
+        if (zombie.isAlive()) {
+            zombie.attackHero(mage);
+        }
     }
 }
+
--- src/com/khomsi/softServe/homework4/TrainingGround.java
+++ src/com/khomsi/softServe/homework4/TrainingGround.java
@@ -4,7 +4,7 @@
 
 package com.khomsi.softServe.homework4;
 
-import com.khomsi.softServe.homework4.character_type.Enemy;
+import com.khomsi.softServe.homework4.enemies.Zombie;
 import com.khomsi.softServe.homework4.heroes.Archer;
 import com.khomsi.softServe.homework4.heroes.Mage;
 import com.khomsi.softServe.homework4.heroes.Warrior;
@@ -18,10 +18,11 @@ public class TrainingGround {
         Warrior warrior = new Warrior("Bob");
         Archer archer = new Archer("Dima");
         Mage mage = new Mage("Vlad");
-        Enemy enemy = new Enemy(100);
+        Zombie zombie = new Zombie(100);
 
-        warrior.attackEnemy(enemy);
-        archer.attackEnemy(enemy);
-        mage.attackEnemy(enemy);
+        warrior.attackEnemy(zombie);
+        archer.attackEnemy(zombie);
+        mage.attackEnemy(zombie);
+        System.out.println(zombie.isAlive() ? "Zombie is still alive" : "Zombie is dead");
     }
 }
